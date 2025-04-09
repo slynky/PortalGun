abf7bbb69882e8b77cca2b8e819d7fbe440b0be3
slynky <kaua5.vs.melo@gmail.com>
2025-04-09 19:06:16 -0300

    Build corrigida

--- src/main/java/com/mod/portalgun/item/ModItems.java
+++ src/main/java/com/mod/portalgun/item/ModItems.java
@@ -2,13 +2,20 @@ package com.mod.portalgun.item;
 
 import com.mod.portalgun.PortalGun;
 import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
 
-    //cria o registro em que os items serao registrados
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, PortalGun.MODID);
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, PortalGun.MODID);
 
+    public static final RegistryObject<Item> URANIUM = ITEMS.register("uranium",
+            () -> new Item(new Item.Properties()));
 
+    public static void registrar(IEventBus bus) {
+        ITEMS.register(bus);
+    }
 }
