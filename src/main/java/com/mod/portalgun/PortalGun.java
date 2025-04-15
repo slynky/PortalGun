3e434b612887ffb5648f075481e57598ba8770e9
slynky <kaua5.vs.melo@gmail.com>
2025-04-14 21:13:23 -0300

    Refatoração 2.0, agora funciona

--- src/main/java/com/mod/portalgun/PortalGun.java
+++ src/main/java/com/mod/portalgun/PortalGun.java
@@ -1,7 +1,5 @@
 package com.mod.portalgun;
 
-import com.mod.portalgun.registry.ModRegistries;
-import com.mod.portalgun.registry.ModItems;
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.registries.Registries;
@@ -30,16 +28,78 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
-// O valor aqui deve corresponder a uma entrada no arquivo META-INF/mods.toml
 @Mod("portalgun")
 public class PortalGun {
     public static final String MODID = "portalgun";
-
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
-
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
-
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
+
+    public static final RegistryObject<Block> URANIUM_BLOCK = BLOCKS.register("uranium_block",
+            () -> new Block(BlockBehaviour.Properties.of()
+                    .setId(BLOCKS.key("example_block"))
+                    .mapColor(MapColor.COLOR_LIGHT_GREEN)
+            )
+    );
+
+    public static final RegistryObject<Item> URANIUM_BLOCK_ITEM = ITEMS.register("uranium_block",
+            () -> new BlockItem(URANIUM_BLOCK.get(), new Item.Properties().setId(ITEMS.key("uranium_block")))
+    );
+
+    public static final RegistryObject<Item>URANIUM_ITEM = ITEMS.register("uranium_item",
+            () -> new Item(new Item.Properties()
+                    .setId(ITEMS.key("uranium_item"))
+            )
+    );
+
+    public static final RegistryObject<CreativeModeTab> PORTALGUN_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
+            .withTabsBefore(CreativeModeTabs.INVENTORY)
+            .icon(() -> URANIUM_ITEM.get().getDefaultInstance())
+            .displayItems((parameters, output) -> {
+                output.accept(URANIUM_ITEM.get());
+            }).build());
+
+    public PortalGun(FMLJavaModLoadingContext context) {
+        IEventBus modEventBus = context.getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+        BLOCKS.register(modEventBus);
+        ITEMS.register(modEventBus);
+        CREATIVE_MODE_TABS.register(modEventBus);
+        MinecraftForge.EVENT_BUS.register(this);
+        modEventBus.addListener(this::addCreative);
+        context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        LOGGER.info("HELLO FROM COMMON SETUP");
+
+        if (Config.logDirtBlock)
+            LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
+
+        LOGGER.info(Config.magicNumberIntroduction + Config.magicNumber);
+
+        Config.items.forEach((item) -> LOGGER.info("ITEM >> {}", item.toString()));
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS)
+            event.accept(URANIUM_BLOCK_ITEM);
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+        LOGGER.info("HELLO from server starting");
+    }
+
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+            LOGGER.info("HELLO FROM CLIENT SETUP");
+            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        }
+    }
 }
--- src/main/java/com/mod/portalgun/registry/ModRegistries.java
+++
@@ -1,7 +0,0 @@
-package com.mod.portalgun.registry;
-
-import net.minecraftforge.eventbus.api.IEventBus;
-
-public class ModRegistries {
-
-}
